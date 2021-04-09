e2cbe267bed09662067f17ff919980fb1e5f470a
g00nd0 <g00nd0@hotmail.com>
2021-04-09 18:29:02 +0800

    Minor change

--- test/sumWithoutHighestAndLowest.test.js
+++ test/sumWithoutHighestAndLowest.test.js
@@ -1,14 +1,9 @@
-const sumWithoutHighestAndLowest = require('../src/sumWithoutHighestAndLowest.js');
+const sumWithoutHighestAndLowest = require("../src/sumWithoutHighestAndLowest.js");
 
-// test cases
+test("array should return 16", function () {
+  expect(sumWithoutHighestAndLowest([6, 2, 1, 8, 10])).toEqual(16);
+});
 
-// e.g. sumWithoutHighestAndLowest([6, 2, 1, 8, 10]) => 16
-// e.g. sumWithoutHighestAndLowest([1, 1, 11, 2, 3]) => 5
-
-test("array should return 16", function() {
-    expect(sumWithoutHighestAndLowest([6, 2, 1, 8, 10])).toEqual(16);
-  });
-  
-  test.skip("array should return 5", function() {
-    expect(sumWithoutHighestAndLowest([1, 1, 11, 2, 3])).toEqual(5);
-  });
+test.skip("array should return 5", function () {
+  expect(sumWithoutHighestAndLowest([1, 1, 11, 2, 3])).toEqual(5);
+});
