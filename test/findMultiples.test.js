061b7afb911cc5e188fa4e0977dccd7818b5c864
g00nd0 <mitchellgoon@hotmail.com>
2021-04-12 11:03:05 +0800

    Add test cases

--- test/findMultiples.test.js
+++ test/findMultiples.test.js
@@ -1,3 +1,27 @@
 const findMultiples = require("../src/findMultiples");
 
+test("array should return [2,4,6]", function () {
+  expect(findMultiples(2, 6)).toEqual([2, 4, 6]);
+});
+
+test("array should return [ 3, 6, 9, 12, 15, 18 ]", function () {
+  expect(findMultiples(3, 18)).toEqual([3, 6, 9, 12, 15, 18]);
+});
+
+test("array should return [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]", function () {
+  expect(findMultiples(5, 59)).toEqual([
+    5,
+    10,
+    15,
+    20,
+    25,
+    30,
+    35,
+    40,
+    45,
+    50,
+    55,
+  ]);
+});
+
 // write test cases to ensure findMultiples() works as expected
