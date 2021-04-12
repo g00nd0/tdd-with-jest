4391685f6d8769cf57b2ab82ea46fa2f8768161f
g00nd0 <g00nd0@hotmail.com>
2021-04-12 09:53:37 +0800

    Complete firstConsectiveNumber and test file, add test case for other test files

--- src/sumWithoutHighestAndLowest.js
+++ src/sumWithoutHighestAndLowest.js
@@ -4,15 +4,19 @@
 // e.g. sumWithoutHighestAndLowest([6, 2, 1, 8, 10]) => 16
 // e.g. sumWithoutHighestAndLowest([1, 1, 11, 2, 3]) => 5
 
-const sumWithoutHighestAndLowest = array => {
+const sumWithoutHighestAndLowest = (array) => {
+  array.sort((a, b) => {
+    return a - b;
+  });
+  const minVals = array[0] * array.filter((num) => num === array[0]).length;
+  const maxVals =
+    array[array.length - 1] *
+    array.filter((num) => num === array[array.length - 1]).length;
+  const total = array.reduce((a, c) => {
+    return a + c;
+  }, -minVals - maxVals);
 
-	array.sort((a,b) => {return a-b})
-	const minVals = array[0] * array.filter(num => num === array[0]).length
-	const maxVals = array[array.length-1] * array.filter(num => num === array[array.length-1]).length
-	const total = array.reduce(((a, c) => {return a+c}), (-minVals-maxVals))
-
-	return(total)
+  return total;
 };
 
-// console.log(sumWithoutHighestAndLowest([6,10, 2, 1, 1, 8, 10]))
 module.exports = sumWithoutHighestAndLowest;
--- test/sumWithoutHighestAndLowest.test.js
+++ test/sumWithoutHighestAndLowest.test.js
@@ -4,6 +4,10 @@ test("array should return 16", function () {
   expect(sumWithoutHighestAndLowest([6, 2, 1, 8, 10])).toEqual(16);
 });
 
-test.skip("array should return 5", function () {
+test("array should return 5", function () {
   expect(sumWithoutHighestAndLowest([1, 1, 11, 2, 3])).toEqual(5);
+});
+
+test("array should return 16", function () {
+  expect(sumWithoutHighestAndLowest([6, 10, 2, 1, 1, 8, 10])).toEqual(5);
 });
