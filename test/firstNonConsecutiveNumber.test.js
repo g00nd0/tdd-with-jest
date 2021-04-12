a3f1b39228a85e2c390501f1705f8c0ecfa280e3
g00nd0 <mitchellgoon@hotmail.com>
2021-04-12 10:36:41 +0800

    Edit test file

--- test/firstNonConsecutiveNumber.test.js
+++ test/firstNonConsecutiveNumber.test.js
@@ -17,8 +17,3 @@ test("array should return null", function () {
 });
 
 // write test cases to ensure firstNonConsecutiveNumber() works as expected
-
-firstNonConsecutiveNumber([1, 3]); // return 3
-firstNonConsecutiveNumber([1, 2, 4, 5, 6]); // return 4
-firstNonConsecutiveNumber([-1, 0, 1, -10]); // return -10
-firstNonConsecutiveNumber([1, 2, 3, 4, 5, 6]); // return null
