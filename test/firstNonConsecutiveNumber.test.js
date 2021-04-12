15c4d05897cdee3e84f67d021e37dfec6da9f4c4
g00nd0 <g00nd0@hotmail.com>
2021-04-12 09:57:34 +0800

    Add test cases for firstNonConsecutiveNumber

--- test/firstNonConsecutiveNumber.test.js
+++ test/firstNonConsecutiveNumber.test.js
@@ -1,3 +1,24 @@
 const firstNonConsecutiveNumber = require("../src/firstNonConsecutiveNumber");
 
+test("array should return 3", function () {
+  expect(firstNonConsecutiveNumber([1, 3])).toEqual(16);
+});
+
+test("array should return 4", function () {
+  expect(firstNonConsecutiveNumber([1, 2, 4, 5, 6])).toEqual(5);
+});
+
+test("array should return -10", function () {
+  expect(firstNonConsecutiveNumber([-1, 0, 1, -10])).toEqual(5);
+});
+
+test("array should return null", function () {
+  expect(firstNonConsecutiveNumber([1, 2, 3, 4, 5, 6])).toEqual(5);
+});
+
 // write test cases to ensure firstNonConsecutiveNumber() works as expected
+
+firstNonConsecutiveNumber([1, 3]); // return 3
+firstNonConsecutiveNumber([1, 2, 4, 5, 6]); // return 4
+firstNonConsecutiveNumber([-1, 0, 1, -10]); // return -10
+firstNonConsecutiveNumber([1, 2, 3, 4, 5, 6]); // return null
